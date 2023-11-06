fd415b7f2170526323f11c66cbeb7c9191fddf47
Anastasiya <127428946+aleshchenko37@users.noreply.github.com>
2023-11-06 12:59:30 +0300

    codeStyle: правки codeStyle

--- src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -54,7 +54,7 @@ public class BookingServiceImplTest {
         Mockito.when(itemRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(item));
         Mockito.when(bookingRepository.save(Mockito.any())).thenReturn(booking);
 
-        BookingDtoFull bookingSaved = bookingService.createBooking(dto, 2l);
+        BookingDtoFull bookingSaved = bookingService.createBooking(dto, 2L);
 
         assertThat(bookingSaved.getId(), notNullValue());
         assertThat(bookingSaved.getStart(), equalTo(dto.getStart()));
@@ -65,7 +65,7 @@ public class BookingServiceImplTest {
 
     @Test
     public void getBooking() {
-        User user = new User(1L,"Anastasiya","ana@mail.ru");
+        User user = new User(1L, "Anastasiya", "ana@mail.ru");
         Item item = new Item(1L, "item", "description", true, user, new ItemRequest());
         Booking booking = new Booking(1L, LocalDateTime.of(2023, 11, 6, 23, 30), LocalDateTime.of(2023, 11, 6, 23, 50), item, user, Status.WAITING);
         BookingDtoFull dto = new BookingDtoFull(1L, LocalDateTime.of(2023, 11, 6, 23, 30), LocalDateTime.of(2023, 11, 6, 23, 50), ItemMapper.toItemDtoForBooking(item), UserMapper.toUserDtoForBooking(user), Status.WAITING);
@@ -73,13 +73,13 @@ public class BookingServiceImplTest {
         Mockito.when(bookingRepository.existsById(Mockito.anyLong())).thenReturn(true);
         Mockito.when(bookingRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(booking));
 
-        assertThat(bookingService.getBooking(1L, 1L),equalTo(BookingMapper.toBookingDtoFull(booking, item)));
+        assertThat(bookingService.getBooking(1L, 1L), equalTo(BookingMapper.toBookingDtoFull(booking, item)));
     }
 
     @Test
     public void getBookingThrowsException() {
-        User owner = new User(1L,"Anastasiya","ana@mail.ru");
-        User renter = new User(2L,"notAnastasiya","notAna@mail.ru");
+        User owner = new User(1L, "Anastasiya", "ana@mail.ru");
+        User renter = new User(2L, "notAnastasiya", "notAna@mail.ru");
         Item item = new Item(1L, "test", "test", true, owner, new ItemRequest());
         Booking booking = new Booking(1L, LocalDateTime.of(2023, 11, 6, 23, 30), LocalDateTime.of(2023, 11, 6, 23, 50), item, renter, Status.WAITING);
 
